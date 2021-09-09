c0f861813136fd01d971396b14ff4ce6131646a4
LompeBoer <88581138+LompeBoer@users.noreply.github.com>
2021-09-09 21:56:39 +0200

    add version flag

--- cmd/wh-dashboard/main.go
+++ cmd/wh-dashboard/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/LompeBoer/wh-dashboard/web"
 )
 
+const VersionNumber = "0.1-alpha"
+
 func main() {
-	log.Printf("Running wh-dashboard (v%s)\n", "0.1-alpha")
+	versionFlag := flag.Bool("version", false, "Print the current version and exit")
+	flag.Parse()
+
+	switch {
+	case *versionFlag:
+		printVersion()
+		return
+	}
+
+	log.Printf("Running wh-dashboard (v%s)\n", VersionNumber)
 
 	handler := web.AssetHandler("/", "build")
 
@@ -16,4 +29,8 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printVersion() {
+	fmt.Println(VersionNumber)
 }
